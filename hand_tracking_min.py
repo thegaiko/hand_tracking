34ceb8ceade8858a6fcbb2786a1e694336a12160
gewwworg <gevor.02@gmail.com>
2022-11-25 13:16:41 +0300

    Hand tracking module

--- hand_tracking_min.py
+++ hand_tracking_min.py
@@ -11,8 +11,6 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-
-
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -21,15 +19,17 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                #print(id, lm)
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(id, cx, cy)
+                # if id == 0:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
